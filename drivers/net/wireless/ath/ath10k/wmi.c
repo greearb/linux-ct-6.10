2eddaaaa16e90e8777f3b42b90c20d6e2bc3088e
Ben Greear <greearb@candelatech.com>
2016-03-09 17:29:05 -0800

    ath10k: Fix CT ratemask handling for 4x4 NICs.
    Several bugs:  First, have to extend each of the peer-assoc-complete
    commands individually since otherwise we are out of sync with the
    various firmware versions.
    
    Second, don't hard-code rate-table as being 3x3.  Instead, use
    the NSS reported by the firmware to know if 3x3 vs 4x4.
    
    Third, VHT mcs entries are 16-bits, but only 10 of them
    are used.
    
    NOTE:  A good bit of this was backported to earlier patch,
    but the backport missed some of the logic in this patch.
    Move this back in the tree and combine with earlier
    rate-set patch.
    
    NOTE2:  Actually trying to enable the larger size for wave-2
    breaks performance.  Must be more bugs or mis-merge somewhere.
    Will comment that out so bisect of basic tput works OK, will
    fix root cause later.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -8015,7 +8015,7 @@ static struct sk_buff *
 ath10k_wmi_10_1_op_gen_peer_assoc(struct ath10k *ar,
 				  const struct wmi_peer_assoc_complete_arg *arg)
 {
-	size_t len = sizeof(struct wmi_10_1_peer_assoc_complete_cmd);
+	size_t len = sizeof(struct wmi_10_1_peer_assoc_complete_cmd_ct);
 	struct sk_buff *skb;
 	int ret;
 
@@ -8041,7 +8041,7 @@ static struct sk_buff *
 ath10k_wmi_10_2_op_gen_peer_assoc(struct ath10k *ar,
 				  const struct wmi_peer_assoc_complete_arg *arg)
 {
-	size_t len = sizeof(struct wmi_10_2_peer_assoc_complete_cmd);
+	size_t len = sizeof(struct wmi_10_2_peer_assoc_complete_cmd_ct);
 	struct sk_buff *skb;
 	int ret;
 
@@ -8066,6 +8066,7 @@ static struct sk_buff *
 ath10k_wmi_10_4_op_gen_peer_assoc(struct ath10k *ar,
 				  const struct wmi_peer_assoc_complete_arg *arg)
 {
+	// TODO:  size_t len = sizeof(struct wmi_10_4_peer_assoc_complete_cmd_ct);
 	size_t len = sizeof(struct wmi_10_4_peer_assoc_complete_cmd);
 	struct sk_buff *skb;
 	int ret;
