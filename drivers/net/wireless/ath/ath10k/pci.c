984ab437351448528e4516d7e3adb04cdb40f719
Ben Greear <greearb@candelatech.com>
2017-05-11 14:05:13 -0700

    ath10k: Fix kernel crash when firmware crashes in OTP.
    It was dereferencing null pointers in that case.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/pci.c
+++ drivers/net/wireless/ath/ath10k/pci.c
@@ -1461,7 +1461,7 @@ static void ath10k_pci_dump_bss_ram(struct ath10k *ar,
 
 	lockdep_assert_held(&ar->data_lock);
 
-	if (!ar->running_fw->fw_file.ram_bss_addr)
+	if (!(ar->running_fw && ar->running_fw->fw_file.ram_bss_addr))
 		return;
 
 	if (!ar->running_fw->fw_file.ram_bss_len)
@@ -1486,7 +1486,7 @@ static void ath10k_pci_dump_bss_rom(struct ath10k *ar,
 
 	lockdep_assert_held(&ar->data_lock);
 
-	if (!ar->running_fw->fw_file.rom_bss_addr)
+	if (!(ar->running_fw && ar->running_fw->fw_file.rom_bss_addr))
 		return;
 
 	if (!ar->running_fw->fw_file.rom_bss_len)
