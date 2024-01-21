fc1be49a0e7199112f967a5d08c69f98fcd2d462
Andriy Utkin <andriy_utkin@fastmail.com>
2024-01-21 23:03:10 +0000

    Fix building without CONFIG_MAC80211_DEBUGFS
      CC      net/mac80211/util.o
    net/mac80211/util.c: In function ‘ieee80211_reconfig’:
    net/mac80211/util.c:2785:30: error: ‘struct ieee80211_local’ has no member named ‘is_dead’
    
    Fixes: f7abd118afb00 (mac80211: add debugfs hw_dead for unrecoverable firmware crashes.)

--- net/mac80211/util.c
+++ net/mac80211/util.c
@@ -1923,7 +1923,9 @@ int ieee80211_reconfig(struct ieee80211_local *local)
 			 * to notify user-space or set debugfs flag to WDT can be kicked in non-attended
 			 * devices such as APs...
 			 */
+#ifdef CONFIG_MAC80211_DEBUGFS
 			local->is_dead = 1;
+#endif
 		}
 		ieee80211_handle_reconfig_failure(local);
 		return res;
