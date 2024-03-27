de1521d7a086c5eb5f3502390bce4b494801e083
Ben Greear <greearb@candelatech.com>
2024-03-27 11:01:38 -0700

    wifi: mt76: mt7925: Support 4 VSTA per radio.
    This builds on the earlier patch that allows mod-param to
    disable th FW_CAP_CNM feature (which allows 7921/7922 to
    do 4 VSTA).
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt792x_core.c
+++ drivers/net/wireless/mediatek/mt76/mt792x_core.c
@@ -956,7 +956,7 @@ struct ieee80211_ops *
 mt792x_get_mac80211_ops(struct device *dev,
 			const struct ieee80211_ops *mac80211_ops,
 			void *drv_data, u8 *fw_features,
-			bool can_disable_fw_cap_cnm)
+			bool can_disable_fw_cap_cnm, bool reassign_ops)
 {
 	struct ieee80211_ops *ops;
 
@@ -967,7 +967,7 @@ mt792x_get_mac80211_ops(struct device *dev,
 
 	*fw_features = mt792x_get_offload_capability(dev, drv_data,
 						     can_disable_fw_cap_cnm);
-	if (!(*fw_features & MT792x_FW_CAP_CNM)) {
+	if (reassign_ops && !(*fw_features & MT792x_FW_CAP_CNM)) {
 		ops->remain_on_channel = NULL;
 		ops->cancel_remain_on_channel = NULL;
 		ops->add_chanctx = ieee80211_emulate_add_chanctx;
