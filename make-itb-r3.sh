912b5f5accc40bae22eda0896af0e593ebaaa6a4
James Courtier-Dutton <james.dutton@gmail.com>
2024-04-11 21:16:54 +0100

    Scripts to make the .itb boot file. Place the bpi-r4.itb in the /boot directory of the BPI-R4. The .itb file contains the Linux kernel and Device tree files.
    Signed-off-by: James Courtier-Dutton <james.dutton@gmail.com>

---
+++ make-itb-r3.sh
@@ -0,0 +1,16 @@
+# For BPI-R3
+uimagearch=arm
+IMAGE=arch/arm64/boot/Image
+LADDR=40080000
+ENTRY=40080000
+kernver=$(make kernelversion | grep -v 'make')
+gitbranch=
+board=bpi-r3
+ARCH=arm64
+
+
+mkimage -A ${uimagearch} -O linux -T kernel -C none -a $LADDR -e $ENTRY -n "Linux Kernel $kernver$gitbranch" -d $IMAGE ./uImage_nodt
+sed "s/%version%/$kernver$gitbranch/" ${board}.its > ${board}.its.tmp
+mkimage -f ${board}.its.tmp ${board}.itb
+cp ${board}.itb ${board}-$kernver$gitbranch.itb
+rm ${board}.its.tmp
