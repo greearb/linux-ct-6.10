12559bcae1a0f23f125407f7b5ce59a7df940c06
James Courtier-Dutton <james.dutton@gmail.com>
2024-05-06 00:34:19 +0100

    Add "config-sdg-8734v" and "make-itb-adtran.sh"
    Signed-off-by: James Courtier-Dutton <james.dutton@gmail.com>

---
+++ make-itb-adtran.sh
@@ -0,0 +1,16 @@
+# For ADTRAN
+uimagearch=arm
+IMAGE=arch/arm64/boot/Image
+LADDR=40080000
+ENTRY=40080000
+kernver=$(make kernelversion | grep -v 'make')
+gitbranch=
+board=adtran
+ARCH=arm64
+bootfile=fit-multi
+
+mkimage -A ${uimagearch} -O linux -T kernel -C none -a $LADDR -e $ENTRY -n "Linux Kernel $kernver$gitbranch" -d $IMAGE ./uImage_nodt
+sed "s/%version%/$kernver$gitbranch/" ${board}.its > ${board}.its.tmp
+mkimage -f ${board}.its.tmp ${bootfile}.itb
+cp ${bootfile}.itb ${board}-$kernver$gitbranch.itb
+rm ${board}.its.tmp
