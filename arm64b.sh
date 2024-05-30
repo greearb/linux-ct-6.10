b2bf77860f3b834f267d69caacb0385673a2cf68
James Courtier-Dutton <james.dutton@gmail.com>
2024-05-30 23:41:43 +0100

    Add some build scripts for Adtran SDG-8734v and SDG-8733v
    Signed-off-by: James Courtier-Dutton <james.dutton@gmail.com>

--- arm64b.sh
+++ arm64b.sh
@@ -1,8 +1,10 @@
 # Script to copy the boot and modules to another folder
-dst="bpi-r4-3"
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+dst="adtran05"
 mkdir /usr/src/$dst
 mkdir /usr/src/$dst/rootfs
 mkdir /usr/src/$dst/boot
 make -j12
 INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=/usr/src/$dst/rootfs make -j4 modules_install
-cp -a bpi-r4.itb /usr/src/$dst/boot
+cp -a fit-multi.itb /usr/src/$dst/boot
