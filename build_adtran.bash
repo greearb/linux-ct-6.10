82547573bdb7031b77229712d1b0f14f6e1d783c
Ben Greear <greearb@candelatech.com>
2024-06-03 13:56:53 -0700

    Build multi-target adtran itb, and check for build failures.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- build_adtran.bash
+++ build_adtran.bash
@@ -10,18 +10,19 @@ KVER_PKG=$VERSION.$PATCHLEVEL.$SUBLEVEL$EXTRAVERSION+
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-make -j32
+make -j32 || exit 1
 
 dst="adtran-w7"
 sudo rm -fr $dst
 mkdir -p $dst/boot/
 mkdir -p $dst/lib/modules/
 
-INSTALL_MOD_PATH=$dst make -j4 modules_install
+INSTALL_MOD_PATH=$dst make -j4 modules_install || exit 2
 
-./make-itb-adtran.sh
+#./make-itb-adtran-sdg-8733v.sh
+./make-itb-adtran-sdg-all.sh || exit 3
 
-cp -a fit-multi.itb $dst/boot/
+cp -a fit-multi.itb $dst/boot/ || exit 4
 
 # Copy firmware
 mkdir -p $dst/lib/firmware/mediatek
