greearb/linux-6.4-be200

wifi: mt76: mt7921: Adjust vht caps when nss changes.
This should fix NSTS caps when changing NSS.
Modeled on how mt7915 does this logic.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -408,6 +408,28 @@ static int mt7921_init_wcid(struct mt7921_dev *dev)
 	return 0;
 }
 
+void mt7921_set_stream_vht_txbf_caps(struct mt7921_phy *phy)
+{
+	int sts;
+	u32 *cap;
+
+	if (!phy->mt76->cap.has_5ghz)
+		return;
+
+	sts = hweight8(phy->mt76->chainmask);
+	cap = &phy->mt76->sband_5g.sband.vht_cap.cap;
+
+	*cap &= ~(IEEE80211_VHT_CAP_BEAMFORMEE_STS_MASK |
+		  IEEE80211_VHT_CAP_SOUNDING_DIMENSIONS_MASK |
+		  IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE |
+		  IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE);
+
+	*cap |= IEEE80211_VHT_CAP_SU_BEAMFORMEE_CAPABLE |
+		IEEE80211_VHT_CAP_MU_BEAMFORMEE_CAPABLE |
+		FIELD_PREP(IEEE80211_VHT_CAP_BEAMFORMEE_STS_MASK,
+			   sts - 1);
+}
+
 static void mt7921_init_work(struct work_struct *work)
 {
 	struct mt7921_dev *dev = container_of(work, struct mt7921_dev,
@@ -419,6 +441,7 @@ static void mt7921_init_work(struct work_struct *work)
 		return;
 
 	mt76_set_stream_caps(&dev->mphy, true);
+	mt7921_set_stream_vht_txbf_caps(&dev->phy);
 	mt7921_set_stream_he_caps(&dev->phy);
 
 	ret = mt76_register_device(&dev->mt76, true, mt76_rates,

drivers/net/wireless/mediatek/mt76/mt7921/mt7921.h
@@ -511,6 +511,7 @@ void mt7921_queue_rx_skb(struct mt76_dev *mdev, enum mt76_rxq_id q,
 			 struct sk_buff *skb, u32 *info);
 void mt7921_stats_work(struct work_struct *work);
 void mt7921_set_stream_he_caps(struct mt7921_phy *phy);
+void mt7921_set_stream_vht_txbf_caps(struct mt7921_phy *phy);
 void mt7921_update_channel(struct mt76_phy *mphy);
 int mt7921_init_debugfs(struct mt7921_dev *dev);
 

greearb/linux-ct-6.10

drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -224,6 +224,28 @@ static int mt7921_init_hardware(struct mt792x_dev *dev)
 	return 0;
 }
 
+void mt7921_set_stream_vht_txbf_caps(struct mt792x_phy *phy)
+{
+	int sts;
+	u32 *cap;
+
+	if (!phy->mt76->cap.has_5ghz)
+		return;
+
+	sts = hweight8(phy->mt76->chainmask);
+	cap = &phy->mt76->sband_5g.sband.vht_cap.cap;
+
+	*cap &= ~(IEEE80211_VHT_CAP_BEAMFORMEE_STS_MASK |
+		  IEEE80211_VHT_CAP_SOUNDING_DIMENSIONS_MASK |
+		  IEEE80211_VHT_CAP_SU_BEAMFORMER_CAPABLE |
+		  IEEE80211_VHT_CAP_MU_BEAMFORMER_CAPABLE);
+
+	*cap |= IEEE80211_VHT_CAP_SU_BEAMFORMEE_CAPABLE |
+		IEEE80211_VHT_CAP_MU_BEAMFORMEE_CAPABLE |
+		FIELD_PREP(IEEE80211_VHT_CAP_BEAMFORMEE_STS_MASK,
+			   sts - 1);
+}
+
 static void mt7921_init_work(struct work_struct *work)
 {
 	struct mt792x_dev *dev = container_of(work, struct mt792x_dev,
@@ -235,6 +257,7 @@ static void mt7921_init_work(struct work_struct *work)
 		return;
 
 	mt76_set_stream_caps(&dev->mphy, true);
+	mt7921_set_stream_vht_txbf_caps(&dev->phy);
 	mt7921_set_stream_he_caps(&dev->phy);
 
 	ret = mt76_register_device(&dev->mt76, true, mt76_rates,

drivers/net/wireless/mediatek/mt76/mt7921/mt7921.h
@@ -262,6 +262,7 @@ bool mt7921_rx_check(struct mt76_dev *mdev, void *data, int len);
 void mt7921_queue_rx_skb(struct mt76_dev *mdev, enum mt76_rxq_id q,
 			 struct sk_buff *skb, u32 *info);
 void mt7921_stats_work(struct work_struct *work);
+void mt7921_set_stream_vht_txbf_caps(struct mt792x_phy *phy);
 void mt7921_set_stream_he_caps(struct mt792x_phy *phy);
 int mt7921_init_debugfs(struct mt792x_dev *dev);
 