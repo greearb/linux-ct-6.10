greearb/linux-ct-6.6

wifi: mt75: mt7921: retry dma-disable on startup.
My system with lots of mtk7921k radios in it often fails to bring up
a few of them.  Adding a retry to the dma-disable logic appears to
fix the problem:

[   10.774941] mt7921e 0000:1d:00.0: ASIC revision: 79610010
[   10.776284] mt7921e 0000:1d:00.0: mt7921_dma_disable failed with timeout, force: 1
[   10.776285] mt7921e 0000:1d:00.0: mt7921_dma_disable failed: -110 (try 0/3)
[   10.778282] mt7921e 0000:1c:00.0: HW/SW Version: 0x8a108a10, Build Time: 20230117170855a
[   10.788427] mt7921e 0000:1c:00.0: WM Firmware Version: ____010000, Build Time: 20230117170942
[   10.852198] mt7921e 0000:1d:00.0: HW/SW Version: 0x8a108a10, Build Time: 20230117170855a
[   10.862222] mt7921e 0000:1d:00.0: WM Firmware Version: ____010000, Build Time: 20230117170942

Add additional logging to find other errors that could keep the radio from working
at all.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -263,12 +263,16 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	u16 cmd;
 
 	ret = pcim_enable_device(pdev);
-	if (ret)
+	if (ret) {
+		pr_info("pcim_enable_device failed: %d\n", ret);
 		return ret;
+	}
 
 	ret = pcim_iomap_regions(pdev, BIT(0), pci_name(pdev));
-	if (ret)
+	if (ret) {
+		pr_info("pcim_iomap_regions failed: %d\n", ret);
 		return ret;
+	}
 
 	pci_read_config_word(pdev, PCI_COMMAND, &cmd);
 	if (!(cmd & PCI_COMMAND_MEMORY)) {
@@ -278,12 +282,16 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	pci_set_master(pdev);
 
 	ret = pci_alloc_irq_vectors(pdev, 1, 1, PCI_IRQ_ALL_TYPES);
-	if (ret < 0)
+	if (ret < 0) {
+		pr_info("pci_alloc_irq_vectors failed: %d\n", ret);
 		return ret;
+	}
 
 	ret = dma_set_mask(&pdev->dev, DMA_BIT_MASK(32));
-	if (ret)
+	if (ret) {
+		pr_info("dma_set_mask failed: %d\n", ret);
 		goto err_free_pci_vec;
+	}
 
 	if (mt7921_disable_aspm)
 		mt76_pci_disable_aspm(pdev);
@@ -297,6 +305,7 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 
 	mdev = mt76_alloc_device(&pdev->dev, sizeof(*dev), ops, &drv_ops);
 	if (!mdev) {
+		pr_info("mt76_alloc_device failed (ENOMEM?)\n");
 		ret = -ENOMEM;
 		goto err_free_pci_vec;
 	}
@@ -316,6 +325,7 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	bus_ops = devm_kmemdup(dev->mt76.dev, dev->bus_ops, sizeof(*bus_ops),
 			       GFP_KERNEL);
 	if (!bus_ops) {
+		dev_info(mdev->dev, "devm_kmemdup bus-opps failed (ENOMEM?)\n");
 		ret = -ENOMEM;
 		goto err_free_dev;
 	}
@@ -330,8 +340,10 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 		goto err_free_dev;
 
 	ret = __mt7921e_mcu_drv_pmctrl(dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "__mt7921e_mcu_drv_pmctrl failed: %d\n", ret);
 		goto err_free_dev;
+	}
 
 	mdev->rev = (mt7921_l1_rr(dev, MT_HW_CHIPID) << 16) |
 		    (mt7921_l1_rr(dev, MT_HW_REV) & 0xff);
@@ -347,16 +359,22 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 
 	ret = devm_request_irq(mdev->dev, pdev->irq, mt7921_irq_handler,
 			       IRQF_SHARED, KBUILD_MODNAME, dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "devm_request_irq failed: %d\n", ret);
 		goto err_free_dev;
+	}
 
 	ret = mt7921_dma_init(dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "mt7921_dma_init failed: %d\n", ret);
 		goto err_free_irq;
+	}
 
 	ret = mt7921_register_device(dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "mt7921_register_device failed failed: %d\n", ret);
 		goto err_free_irq;
+	}
 
 	return 0;
 

greearb/linux-ct-6.10

drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -276,12 +276,16 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	int ret;
 
 	ret = pcim_enable_device(pdev);
-	if (ret)
+	if (ret) {
+		pr_info("pcim_enable_device failed: %d\n", ret);
 		return ret;
+	}
 
 	ret = pcim_iomap_regions(pdev, BIT(0), pci_name(pdev));
-	if (ret)
+	if (ret) {
+		pr_info("pcim_iomap_regions failed: %d\n", ret);
 		return ret;
+	}
 
 	pci_read_config_word(pdev, PCI_COMMAND, &cmd);
 	if (!(cmd & PCI_COMMAND_MEMORY)) {
@@ -291,12 +295,16 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	pci_set_master(pdev);
 
 	ret = pci_alloc_irq_vectors(pdev, 1, 1, PCI_IRQ_ALL_TYPES);
-	if (ret < 0)
+	if (ret < 0) {
+		pr_info("pci_alloc_irq_vectors failed: %d\n", ret);
 		return ret;
+	}
 
 	ret = dma_set_mask(&pdev->dev, DMA_BIT_MASK(32));
-	if (ret)
+	if (ret) {
+		pr_info("dma_set_mask failed: %d\n", ret);
 		goto err_free_pci_vec;
+	}
 
 	if (mt7921_disable_aspm)
 		mt76_pci_disable_aspm(pdev);
@@ -310,6 +318,7 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 
 	mdev = mt76_alloc_device(&pdev->dev, sizeof(*dev), ops, &drv_ops);
 	if (!mdev) {
+		pr_info("mt76_alloc_device failed (ENOMEM?)\n");
 		ret = -ENOMEM;
 		goto err_free_pci_vec;
 	}
@@ -330,6 +339,7 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	bus_ops = devm_kmemdup(dev->mt76.dev, dev->bus_ops, sizeof(*bus_ops),
 			       GFP_KERNEL);
 	if (!bus_ops) {
+		dev_info(mdev->dev, "devm_kmemdup bus-opps failed (ENOMEM?)\n");
 		ret = -ENOMEM;
 		goto err_free_dev;
 	}
@@ -344,8 +354,10 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 		goto err_free_dev;
 
 	ret = __mt792xe_mcu_drv_pmctrl(dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "__mt792xe_mcu_drv_pmctrl failed: %d\n", ret);
 		goto err_free_dev;
+	}
 
 	chipid = mt7921_l1_rr(dev, MT_HW_CHIPID);
 	if (chipid == 0x7961 && (mt7921_l1_rr(dev, MT_HW_BOUND) & BIT(7)))
@@ -364,16 +376,22 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 
 	ret = devm_request_irq(mdev->dev, pdev->irq, mt792x_irq_handler,
 			       IRQF_SHARED, KBUILD_MODNAME, dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "devm_request_irq failed: %d\n", ret);
 		goto err_free_dev;
+	}
 
 	ret = mt7921_dma_init(dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "mt7921_dma_init failed: %d\n", ret);
 		goto err_free_irq;
+	}
 
 	ret = mt7921_register_device(dev);
-	if (ret)
+	if (ret) {
+		dev_info(mdev->dev, "mt7921_register_device failed failed: %d\n", ret);
 		goto err_free_irq;
+	}
 
 	if (of_property_read_bool(dev->mt76.dev->of_node, "wakeup-source"))
 		device_init_wakeup(dev->mt76.dev, true);